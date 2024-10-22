87226d4d2e13987b863899fea7e8a930e327236f
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-22 19:18:55 +0200

    feat: add function to take user input and store new book objects in library array

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Library object
+
+const myLibrary = [];
+
 //Object constructor for books
 
 function Book(title, author, pages, read) {
@@ -7,9 +11,20 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-theHobbit = new Book("The Hobbit", "Tolkien", 295, "read");
-
 Book.prototype.info = function () {
   return `${this.title} by ${this.author}. ${this.pages} pages. ${this.read}.`;
 };
+
+//testing book object constructor
+theHobbit = new Book("The Hobbit", "Tolkien", 295, "read");
 console.log(theHobbit.info());
+
+//add book to library
+function addBookToLibrary() {
+  let title = prompt("Enter title");
+  let author = prompt("Enter author");
+  let pages = prompt("Enter number of pages");
+  let read = prompt("Read or not read?");
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
